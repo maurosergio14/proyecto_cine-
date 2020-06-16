b1e672b9160d67d59c0c16683b4178da4cc211a7
Thomas <Thomas@Thomas-PC>
2020-06-16 18:36:21 -0300

    Merge origin/master subida de la clase FuncionVerPelicula

---
+++ src/MODELO/FuncionVerPelicula.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MODELO;
+
+/**
+ *
+ * @author Thomas
+ */
+public class FuncionVerPelicula {
+    private int id_verPelicula;
+    private Pelicula id_pelicula;
+    private Sala id_sala;
+    private String horario_desde;
+    private String horario_hasta;
+
+    public FuncionVerPelicula(Pelicula id_pelicula, Sala id_sala, String horario_desde, String horario_hasta) {
+        this.id_pelicula = id_pelicula;
+        this.id_sala = id_sala;
+        this.horario_desde = horario_desde;
+        this.horario_hasta = horario_hasta;
+    }
+
+    public int getId_verPelicula() {
+        return id_verPelicula;
+    }
+
+    public void setId_verPelicula(int id_verPelicula) {
+        this.id_verPelicula = id_verPelicula;
+    }
+
+    public Pelicula getId_pelicula() {
+        return id_pelicula;
+    }
+
+    public void setId_pelicula(Pelicula id_pelicula) {
+        this.id_pelicula = id_pelicula;
+    }
+
+    public Sala getId_sala() {
+        return id_sala;
+    }
+
+    public void setId_sala(Sala id_sala) {
+        this.id_sala = id_sala;
+    }
+
+    public String getHorario_hasta() {
+        return horario_hasta;
+    }
+
+    public void setHorario_hasta(String horario_hasta) {
+        this.horario_hasta = horario_hasta;
+    }
+
+    @Override
+    public String toString() {
+        return "FuncionVerPelicula{" + "id_verPelicula=" + id_verPelicula + ", id_pelicula=" + id_pelicula + ", id_sala=" + id_sala + ", horario_desde=" + horario_desde + ", horario_hasta=" + horario_hasta + '}';
+    }
+    
+}
