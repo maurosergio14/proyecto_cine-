20a097de4db7eec099775bdb0f43dd426461dba4
luis-1997 <56975017+luis-1997@users.noreply.github.com>
2020-06-23 16:33:35 -0300

    Add files via upload

--- src/MODELO/FuncionVerPelicula.java
+++ src/MODELO/FuncionVerPelicula.java
@@ -1,23 +1,36 @@
 /*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
  */
 package MODELO;
+
+import java.sql.Time;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
  * @author Thomas
  */
 public class FuncionVerPelicula {
-    private int id_verPelicula;
-    private Pelicula id_pelicula;
-    private Sala id_sala;
-    private String horario_desde;
-    private String horario_hasta;
 
-    public FuncionVerPelicula(Pelicula id_pelicula, Sala id_sala, String horario_desde, String horario_hasta) {
-        this.id_pelicula = id_pelicula;
-        this.id_sala = id_sala;
+    private int id_verPelicula = -1;
+
+    private Pelicula pelicula;
+    private Sala sala;
+    private Time horario_desde;
+    private Time horario_hasta;
+    private List<Butaca> butacas = new ArrayList<>();
+
+    public FuncionVerPelicula(Pelicula pelicula, Sala sala, Time horario_desde, Time horario_hasta) {
+        this.pelicula = pelicula;
+        this.sala = sala;
         this.horario_desde = horario_desde;
         this.horario_hasta = horario_hasta;
+    }
+
+    public FuncionVerPelicula() {
     }
 
     public int getId_verPelicula() {
@@ -28,33 +41,53 @@ public class FuncionVerPelicula {
         this.id_verPelicula = id_verPelicula;
     }
 
-    public Pelicula getId_pelicula() {
-        return id_pelicula;
+    public Pelicula getPelicula() {
+        return pelicula;
     }
 
-    public void setId_pelicula(Pelicula id_pelicula) {
-        this.id_pelicula = id_pelicula;
+    public void setPelicula(Pelicula pelicula) {
+        this.pelicula = pelicula;
     }
 
-    public Sala getId_sala() {
-        return id_sala;
+    public Sala getSala() {
+        return sala;
     }
 
-    public void setId_sala(Sala id_sala) {
-        this.id_sala = id_sala;
+    public void setSala(Sala sala) {
+        this.sala = sala;
     }
 
-    public String getHorario_hasta() {
+    public Time getHorario_desde() {
+        return horario_desde;
+    }
+
+    public void setHorario_desde(Time horario_desde) {
+        this.horario_desde = horario_desde;
+    }
+
+    public Time getHorario_hasta() {
         return horario_hasta;
     }
 
-    public void setHorario_hasta(String horario_hasta) {
+    public void setHorario_hasta(Time horario_hasta) {
         this.horario_hasta = horario_hasta;
     }
 
+    public List<Butaca> getButacas() {
+        return butacas;
+    }
+
+    public void setButacas(List<Butaca> butacas) {
+        this.butacas = butacas;
+    }
+
+   
+    
+    
+    
     @Override
     public String toString() {
-        return "FuncionVerPelicula{" + "id_verPelicula=" + id_verPelicula + ", id_pelicula=" + id_pelicula + ", id_sala=" + id_sala + ", horario_desde=" + horario_desde + ", horario_hasta=" + horario_hasta + '}';
+        return "FuncionVerPelicula{" + "id_verPelicula=" + id_verPelicula + ", id_pelicula=" + pelicula + ", id_sala=" + sala + ", horario_desde=" + horario_desde + ", horario_hasta=" + horario_hasta + '}';
     }
-    
+
 }
